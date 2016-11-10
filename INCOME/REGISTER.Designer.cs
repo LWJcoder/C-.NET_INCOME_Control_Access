edf4131cc2bca39dbdb5a9c3fac1fbef0a12f278
LWJgithub <15818464582@163.com>
2016-11-10 23:15:49 +0800

    Add files via upload

---
+++ INCOME/REGISTER.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace INCOME
+{
+    partial class REGISTER
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.psw2 = new System.Windows.Forms.TextBox();
+            this.psw1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.vali1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "注册";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "密码";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "再次输入密码";
+            // 
+            // psw2
+            // 
+            this.psw2.Location = new System.Drawing.Point(101, 135);
+            this.psw2.Name = "psw2";
+            this.psw2.Size = new System.Drawing.Size(100, 21);
+            this.psw2.TabIndex = 3;
+            // 
+            // psw1
+            // 
+            this.psw1.Location = new System.Drawing.Point(101, 82);
+            this.psw1.Name = "psw1";
+            this.psw1.Size = new System.Drawing.Size(100, 21);
+            this.psw1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "用户名";
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(101, 32);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(100, 21);
+            this.username.TabIndex = 6;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(101, 162);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(92, 33);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // vali1
+            // 
+            this.vali1.Location = new System.Drawing.Point(93, 201);
+            this.vali1.Name = "vali1";
+            this.vali1.Size = new System.Drawing.Size(100, 21);
+            this.vali1.TabIndex = 8;
+            // 
+            // REGISTER
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.vali1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.psw1);
+            this.Controls.Add(this.psw2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "REGISTER";
+            this.Text = "REGISTER";
+            this.Load += new System.EventHandler(this.REGISTER_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox psw2;
+        private System.Windows.Forms.TextBox psw1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox vali1;
+    }
+}
---
+++ INCOME/change.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace INCOME
+{
+    public partial class change : Form
+    {
+        public change()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             string psw = password1.Text,
+                psw0 = password2.Text;
+            //得到用户名
+             string username = Form1.strName;
+
+            if (psw != psw0) {
+                MessageBox.Show("两次密码不一致，请重新输入");
+            }
+
+            //注册
+            System.Data.OleDb.OleDbConnection conn = new
+              System.Data.OleDb.OleDbConnection();
+            // TODO: Modify the connection string and include any
+            // additional required properties for your database.
+            conn.ConnectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;" +
+                @"Data source=" +
+                @"USER_TABLE1.mdb";
+            try
+            {
+                string queryString = "UPDATE USER_TABLE SET USER_PSW = '" + psw0 + "' WHERE USER_ID =  '" + username+"'";
+                System.Data.OleDb.OleDbCommand command = new System.Data.OleDb.OleDbCommand(queryString,conn);
+                conn.Open();
+               int i =  command.ExecuteNonQuery();
+
+               if (i == 1)
+               {
+                   this.Close();
+                   MessageBox.Show("修改成功~~");
+               }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to connect to data source" + ex.ToString());
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+    }
+}
